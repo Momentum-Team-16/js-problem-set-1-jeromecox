c234afbb332565fee1ddb097c29542bf716efd34
jeromecox <jeromecox@hotmail.com>
2022-11-08 13:06:33 -0600

    finished

--- homework.js
+++ homework.js
@@ -1,38 +1,69 @@
 // 1. Set the variable `givenName` to the string "Addison".
-
+let givenName = "Addison";
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
-
+let candies = 20;
+let people = 6;
+let leftover = 20 % 6;
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
-
+function greeting(name) {
+  return "Hello, " + name + "!";
+}
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
-
+function isOdd(num) {
+  if (num % 2 === 1 || num % 2 === -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
-
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
-
+function fahrenheitToCelsius(fTemp) {
+  let Celsius = (fTemp - 32) * (5 / 9);
+  return Celsius;
+}
 // 7. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
-
+function celsiusToFahrenheit(cTemp) {
+  let Fahrenheit = cTemp * (9 / 5) + 32;
+  return Fahrenheit;
+}
 // 8. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Kelvin. This function must use your previous
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
-
+function fahrenheitToKelvin(fTemp) {
+  let Celsius = fahrenheitToCelsius(fTemp);
+  let Kelvin = Celsius + 273.15;
+  return Kelvin;
+}
 // 9. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
-
+function lesser(num1, num2) {
+  if (num1 > num2) {
+    return num2;
+  }
+  return num1;
+}
 // 10. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
 // in the specified language. The supported languages and their
@@ -44,7 +75,20 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
-
+function multigreeting(name, language) {
+  if (language == "en") {
+    return "Hello, " + name + "!";
+  }
+  if (language == "es") {
+    return "¡Hola, " + name + "!";
+  }
+  if (language == "fr") {
+    return "Bonjour, " + name + "!";
+  }
+  if (language == "eo") {
+    return "Saluton, " + name + "!";
+  }
+}
 // 11. The greatest common divisor (https://en.wikipedia.org/wiki/Greatest_common_divisor)
 // is the largest integer that, given two other integers, can be divided into them. For
 // example, the greatest common divisor of 24 and 81 is 3. The greatest common divisor of
@@ -71,3 +115,35 @@
 
 // Write a function called gcd that takes two arguments and returns the greatest common
 // divisor using the instructions above.
+// function gcd(a, b) {
+//   d = 0;
+//   if (a == b) {
+//     return a;
+//   } else if (a % 2 === 0 && b % 2 === 0) {
+//     a = a / 2;
+//     b = b / 2;
+//     d++;
+//     g = a;
+//     return g * 2 ** d;
+//   }
+// }
+
+function gcd(a, b) {
+  let d = 0;
+  while (a % 2 === 0 && b % 2 === 0) {
+    a = a / 2;
+    b = b / 2;
+    d++;
+  }
+  while (a != b) {
+    if (a % 2 === 0) {
+      a = a / 2;
+    } else if (b % 2 === 0) {
+      b = b / 2;
+    } else if (a > b) {
+      a = (a - b) / 2;
+    } else b = (b - a) / 2;
+  }
+  let g = a;
+  return g * 2 ** d;
+}
